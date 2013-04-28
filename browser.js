c97bc90112d29ddaa19a9e7131a31271b9fdffe0
Hugh Kennedy <hughskennedy@gmail.com>
2013-04-29 03:42:48 +1000

    update gh-pages

--- browser.js
+++ browser.js
@@ -37931,42 +37931,7 @@ Pointer.prototype.render = function(ctx, manager) {
   ctx.globalAlpha = 1
 }
 
-},{"./helpers":24,"./entity":23,"inherits":16}],10:[function(require,module,exports){
-var inherits = require('inherits')
-  , Entity = require('./entity')
-  , game
-
-module.exports = Camera
-
-function Camera() {
-  if (!(this instanceof Camera)) return new Camera()
-  Entity.call(this)
-  if (!game) throw new Error('game not ready')
-
-  this.pos = [-250,-250]
-}
-inherits(Camera, Entity)
-
-Camera.register = function(g) {
-  game = g
-}
-
-Camera.prototype.tick = function(dt, manager) {
-  var player = manager.first('player')
-    , game = manager.game
-
-  this.pos[0] = this.pos[0] + (player.pos[0] - game.width/2 - this.pos[0]) * 0.05
-  this.pos[1] = this.pos[1] + (player.pos[1] - game.height/2 - this.pos[1]) * 0.05
-}
-
-Camera.prototype.relative = function(pos, arr) {
-  arr = arr || []
-  arr[0] = pos[0] - this.pos[0]
-  arr[1] = pos[1] - this.pos[1]
-  return arr
-}
-
-},{"./entity":23,"inherits":16}],11:[function(require,module,exports){
+},{"./helpers":24,"./entity":23,"inherits":16}],11:[function(require,module,exports){
 var inherits = require('inherits')
   , Entity = require('./entity')
   , imageloaded = require('image-loaded')
@@ -38033,7 +37998,42 @@ Chaser.prototype.render = function(ctx, manager) {
   }
 }
 
-},{"./entity":23,"inherits":16,"image-loaded":25}],12:[function(require,module,exports){
+},{"./entity":23,"inherits":16,"image-loaded":25}],10:[function(require,module,exports){
+var inherits = require('inherits')
+  , Entity = require('./entity')
+  , game
+
+module.exports = Camera
+
+function Camera() {
+  if (!(this instanceof Camera)) return new Camera()
+  Entity.call(this)
+  if (!game) throw new Error('game not ready')
+
+  this.pos = [-250,-250]
+}
+inherits(Camera, Entity)
+
+Camera.register = function(g) {
+  game = g
+}
+
+Camera.prototype.tick = function(dt, manager) {
+  var player = manager.first('player')
+    , game = manager.game
+
+  this.pos[0] = this.pos[0] + (player.pos[0] - game.width/2 - this.pos[0]) * 0.05
+  this.pos[1] = this.pos[1] + (player.pos[1] - game.height/2 - this.pos[1]) * 0.05
+}
+
+Camera.prototype.relative = function(pos, arr) {
+  arr = arr || []
+  arr[0] = pos[0] - this.pos[0]
+  arr[1] = pos[1] - this.pos[1]
+  return arr
+}
+
+},{"./entity":23,"inherits":16}],12:[function(require,module,exports){
 var inherits = require('inherits')
   , Entity = require('./entity')
   , imageloaded = require('image-loaded')
@@ -38298,7 +38298,11 @@ var imageloaded = require('image-loaded')
 
 module.exports = Sky
 
+var gradientLoaded = false
 var gradient = new Image
+imageloaded(gradient, function() {
+  gradientLoaded = true
+})
 gradient.src = 'sky.png'
 
 function Sky() {
@@ -38329,6 +38333,8 @@ Sky.register = function(g) {
 }
 
 Sky.prototype.time = function(time) {
+  if (!gradientLoaded) return 'rgb(99,164,252)'
+
   var data = this.data
     , length = data.length
     , idxA = Math.min(Math.floor(time * length / 4) * 4, length - 4)
